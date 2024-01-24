166aa610fd48653440747f0f55348733023e10de
Paul Burger <iniidabyss@gmail.com>
2024-01-23 19:07:37 -0500

    Updates to frontend as of 7 pm 01/23. Logout function still in development.
    Co-authored-by: Paul Burger <iniidabyss@gmail.com>
    Co-authored-by: Brian Yan <yan.briann@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,59 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import fetchData from "./Functions";
-import Login from "./components/loginPage/Login";
-import HomePage from "./HomePage";
-import "./App.css";
-import { CssBaseline } from "@mui/material";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import fetchData from './Functions';
+import Login from './components/loginPage/Login';
+import HomePage from './HomePage';
+import './App.css';
+import { CssBaseline } from '@mui/material';
+import { useSelector } from 'react-redux';
 
 let showSeeMore = null;
 let fetched = false;
 
-
 const App = () => {
-
   const user = useSelector((state) => state.user);
-  console.log("--------------", user);
+  console.log('user', user);
+  console.log('--------------', user);
   // let startCity = user.city
   // console.log(startCity)
-  const [jobType, setJobType] = useState("Python developer");
-  const [city, setCity] = useState("");
+  const [jobType, setJobType] = useState('Python developer');
+  const [city, setCity] = useState('');
   const [jobCards, updateCards] = useState([]);
+  // PB: Added a piece of state to keep track of searches
+  const [searchCount, updateSearchCount] = useState(0);
 
   // create two variables to hold city and state, then pass as props
   let state;
   let newCity;
 
-
-//search function, when user submits, it iniatites a fetch request, and appends data to the job cards array. Then updates state of cards array
+  //search function, when user submits, it iniatites a fetch request, and appends data to the job cards array. Then updates state of cards array
   const getSearch = async (e) => {
-    const cityArr = city.split(", ");
+    const cityArr = city.split(', ');
     state = cityArr[1];
     newCity = cityArr[0];
+
+    // PB: When getSearch is called, set searchCount variable to 1
+    updateSearchCount(1);
+
     showSeeMore = (
       <button
-        className="see-more-button"
-        type="submit"
+        className='see-more-button'
+        type='submit'
         onClick={async (e) => await updateCount(e)}
       >
         Click for more jobs
       </button>
     );
-    const newData = await fetchData(newCity, state, jobType);
-    
+
+    // PB: variable for search count
+    let count = searchCount;
+    const newData = await fetchData(newCity, state, jobType, count);
+
     // this is why all new data is added to existing old data. instead, just set existing state to newData
-  //  const updatedData = jobCards.concat(newData);
-    const updatedData = newData
+    //  const updatedData = jobCards.concat(newData);
+    const updatedData = newData;
     updateCards(updatedData);
   };
-
 
   //function to set the city to user input , and change job type state
   const updateCity = (e) => {
@@ -56,12 +62,17 @@ const App = () => {
 
   //what is showSeeMore meant to do?? Seems like it fetches data, updates state, and returns showSeeMore? Maybe 0th index is not relevant
   const updateCount = async (e) => {
-    console.log("-------clicked---------");
-    const newData = await fetchData(newCity, state, jobType);
-    updateCards(newData);
+    console.log('-------clicked---------');
 
+    // PB: Update searchCount on state by 1
+    updateSearchCount(searchCount + 1);
+
+    let count = searchCount;
+    const newData = await fetchData(newCity, state, jobType, count);
+
+    updateCards(newData);
     let done = newData[0];
-    if (done === "true") {
+    if (done === 'true') {
       showSeeMore = null;
     }
   };
@@ -71,19 +82,19 @@ const App = () => {
     setJobType(e.target.value);
   };
 
-
   //logic: if not logged in, render log in page, else show home page
-  //passes down state of job, city, state, cards, and search
+  //passes down state of job, city, state, cards, and searchhttp://localhost:3000/
 
   return (
-    <div className="app-bg">
+    <div className='app-bg'>
       <CssBaseline />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route
-          path="/home"
+          path='/home'
           element={
             <HomePage
+              user = {user}
               jobCards={jobCards}
               getSearch={getSearch}
               // setCards = {setCards}
@@ -99,6 +110,6 @@ const App = () => {
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,35 +1,41 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Search = ({ getSearch, setJob, city, updateCity }) => {
+  const user = useSelector((state) => state.user);
+  console.log('city in search', user.city);
   return (
-    <div className="search-bar">
-      <select className="select-box" name="type" id="date-types" onChange={(e) => setJob(e)}>
-        <option value="" disabled selected>
+    <div className='search-bar'>
+      <select
+        className='select-box'
+        name='type'
+        id='date-types'
+        onChange={(e) => setJob(e)}
+      >
+        <option value='' disabled>
           Select Job Type
         </option>
-        <option value="Python developer">Python developer</option>
-        <option value="Javascript developer">Javascript developer</option>
-        <option value="HTML/CSS">HTML/CSS</option>
-        <option value="Full Stack">Full Stack</option>
-        <option value="Mobile iOS">Mobile iOS</option>
-        <option value="Data Analyst">Data Analyst</option>
-        <option value="Software Developer">Software Developer</option>
-        <option value="Security">Security</option>
-        <option value="IT">IT</option>
-        <option value="Quality Assurance">Quality Assurance</option>
-        <option value="Network Engineer">Network Engineer</option>
-
+        <option value='Python developer'>Python developer</option>
+        <option value='Javascript developer'>Javascript developer</option>
+        <option value='HTML/CSS'>HTML/CSS</option>
+        <option value='Full Stack'>Full Stack</option>
+        <option value='Mobile iOS'>Mobile iOS</option>
+        <option value='Data Analyst'>Data Analyst</option>
+        <option value='Software Developer'>Software Developer</option>
+        <option value='Security'>Security</option>
+        <option value='IT'>IT</option>
+        <option value='Quality Assurance'>Quality Assurance</option>
+        <option value='Network Engineer'>Network Engineer</option>
       </select>
       <input
-        className="search-bar select-box"
-        type="text"
-        placeholder="Austin, TX"
+        className='search-bar select-box'
+        type='text'
+        placeholder={user.city}
         onChange={(e) => updateCity(e)}
       />
       <button
-        className="search-button"
-        type="submit"
+        className='search-button'
+        type='submit'
         onClick={async (e) => await getSearch(e)}
       >
         Search
