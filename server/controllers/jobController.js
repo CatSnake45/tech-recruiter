57a8159184dc47ce53902fa035c8e30c53e0e5af
Julien Kerekes <julien.kerekes@gmail.com>
2024-01-24 17:55:02 +0100

    Changed amount of jobs per query to 10
    Co-authored-by: Ari Benkov <akbenkov79@gmail.com>
    Co-authored-by: Brian Yan <BrianYanGitHub@users.noreply.github.com>

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -5,12 +5,12 @@ jobController.getJobData = async (req, res, next) => {
   // let page = 1;
   // let what = 'Javascript Developer';
   // let where = 'Boston';
-  const url = `https://api.adzuna.com/v1/api/jobs/us/search/${page}?app_id=${process.env.API_APP_ID}&app_key=${process.env.API_KEY}&results_per_page=50&what=${what}&where=${where}`;
-  //https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d045d28c&app_key=9762edd919aa95b4dce07ea1e12c2639&results_per_page=200&what=Javascript%20Developer&where=Torrance
-  //what = search term (like 'javascript developer' from our dropdown menu)
-  //where = city/place //input.replace(' ','%20')
-  //distance?
-  //page (where it says /search/1?) (important when loading more jobs!)
+  const url = `https://api.adzuna.com/v1/api/jobs/us/search/${page}?app_id=${process.env.API_APP_ID}&app_key=${process.env.API_KEY}&results_per_page=10&what=${what}&where=${where}`;
+  // https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d045d28c&app_key=9762edd919aa95b4dce07ea1e12c2639&results_per_page=200&what=Javascript%20Developer&where=Torrance
+  // what = search term (like 'javascript developer' from our dropdown menu)
+  // where = city/place //input.replace(' ','%20')
+  // distance?
+  // page (where it says /search/1?) (important when loading more jobs!)
 
   try {
     console.log(url);
