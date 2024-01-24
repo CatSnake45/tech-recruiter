2a5eb8c482716003a32666d5bbc28f948e823bd8
Julien Kerekes <julien.kerekes@gmail.com>
2024-01-24 01:16:13 +0100

    Got backend ready for merging
    Co-authored-by: Ari Benkov <akbenkov79@gmail.com>

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,10 +1,10 @@
 const jobController = {};
 
 jobController.getJobData = async (req, res, next) => {
-  // const { keywords, city, page } = req.body;
-  let page = 1;
-  let what = 'Javascript Developer';
-  let where = 'Boston';
+  const { what, where, page } = req.body;
+  // let page = 1;
+  // let what = 'Javascript Developer';
+  // let where = 'Boston';
   const url = `https://api.adzuna.com/v1/api/jobs/us/search/${page}?app_id=${process.env.API_APP_ID}&app_key=${process.env.API_KEY}&results_per_page=50&what=${what}&where=${where}`;
   //https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d045d28c&app_key=9762edd919aa95b4dce07ea1e12c2639&results_per_page=200&what=Javascript%20Developer&where=Torrance
   //what = search term (like 'javascript developer' from our dropdown menu)
