1e15992a12af880067b28d57ccbd0e8b10aca6d8
Brian <yan.briann@gmail.com>
2024-01-24 16:29:31 -0500

    added roles to button and search, modified welcome message, and added possible default logic

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from './components/Search';
 import JobDisplay from './components/JobDisplay';
 // remove following line if redux not being implemented
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setLogout } from './state';
+import fetchData from './Functions';
 
 const HomePage = ({
   // props being drilled down from App.jsx
@@ -34,6 +35,23 @@ const HomePage = ({
 
   // const { user, setUser } = useState(user);
 
+  // useEffect(() => {
+  //   console.log('in useEffect');
+  //   const fetchDefaultJobs = async () => {
+  //     try {
+  //       const { done, jobsArray } = await fetchData(
+  //         user.city,
+  //         'Python developer',
+  //         1
+  //       );
+  //     } catch (error) {
+  //       console.error('Error fetching default jobs:', error);
+  //     }
+  //   };
+  //   fetchDefaultJobs();
+  //   console.log('result of fetching default', fetchDefaultJobs);
+  // }, [user.city, setJob]);
+
   const handleClick = () => {
     navigate('/');
     dispatch(setLogout());
@@ -45,6 +63,7 @@ const HomePage = ({
         <img src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' />
         <button
           type='button'
+          role='bigBtn'
           className='name-btn-container'
           class='btn-logout btn-outline-primary'
           onClick={handleClick}
@@ -59,11 +78,11 @@ const HomePage = ({
           <p className='homepage-p'>Filter by JOB and LOCATION</p>
           <p className='homepage-p'>
             <strong>Hi, {user.userName}!</strong> Fill out the fields to see
-            jobs in {user.city}.
+            jobs in your desired city
           </p>
           <div className='search-div'>
             <Search
-              jobCards = {jobCards}
+              jobCards={jobCards}
               getSearch={getSearch}
               newCity={newCity}
               setJob={setJob}
